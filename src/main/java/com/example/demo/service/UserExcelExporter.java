d64e4832c852824337ec58f21478ee2ed20c7e03
Mai Gia Truong <truong.mg@gmail.com>
2022-02-15 15:04:57 +0800

    remove kafka usages

--- src/main/java/com/example/demo/service/UserExcelExporter.java
+++ src/main/java/com/example/demo/service/UserExcelExporter.java
@@ -5,6 +5,7 @@ import com.example.demo.kafka.UserReport;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellUtil;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.ByteArrayInputStream;
@@ -16,6 +17,10 @@ import java.util.List;
 public class UserExcelExporter {
 
     public ByteArrayInputStream export(UserReport userReport) {
+        return exportNormalFile(userReport);
+    }
+
+    private ByteArrayInputStream exportNormalFile(UserReport userReport) {
         try (Workbook workbook = new XSSFWorkbook()) {
             Sheet sheet = workbook.createSheet("UserReport");
 
@@ -55,6 +60,60 @@ public class UserExcelExporter {
 
             // Making size of column auto resize to fit with data
             sheet.autoSizeColumn(0);
+//            sheet.autoSizeColumn(1);
+//            sheet.autoSizeColumn(2);
+//            sheet.autoSizeColumn(3);
+//            sheet.autoSizeColumn(4);
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            workbook.write(outputStream);
+            return new ByteArrayInputStream(outputStream.toByteArray());
+        } catch (IOException ex) {
+            log.error("Error during export Excel file", ex);
+            return null;
+        }
+    }
+
+    private ByteArrayInputStream exportHugeFile(UserReport userReport) {
+        try (Workbook workbook = new SXSSFWorkbook(100)) {
+            Sheet sheet = workbook.createSheet("UserReport");
+
+            createHeader(workbook, sheet);
+
+//            Cell cell = row.createCell(0);
+//            cell.setCellValue("First Name");
+//            cell.setCellStyle(headerCellStyle);
+//
+//            cell = row.createCell(1);
+//            cell.setCellValue("Last Name");
+//            cell.setCellStyle(headerCellStyle);
+//
+//            cell = row.createCell(2);
+//            cell.setCellValue("Email");
+//            cell.setCellStyle(headerCellStyle);
+//
+//            cell = row.createCell(3);
+//            cell.setCellValue("Phone Number");
+//            cell.setCellStyle(headerCellStyle);
+//
+//            cell = row.createCell(4);
+//            cell.setCellValue("Address");
+//            cell.setCellStyle(headerCellStyle);
+
+            // Creating data rows for each contact
+//            for(int i = 0; i < contacts.size(); i++) {
+//                Row dataRow = sheet.createRow(i + 1);
+//                dataRow.createCell(0).setCellValue(contacts.get(i).getFirstName());
+//                dataRow.createCell(1).setCellValue(contacts.get(i).getLastName());
+//                dataRow.createCell(2).setCellValue(contacts.get(i).getEmail());
+//                dataRow.createCell(3).setCellValue(contacts.get(i).getPhoneNumber());
+//                dataRow.createCell(4).setCellValue(contacts.get(i).getAddress());
+//            }
+
+            createBody(userReport, sheet);
+
+            // Making size of column auto resize to fit with data
+//            sheet.autoSizeColumn(0);
 //            sheet.autoSizeColumn(1);
 //            sheet.autoSizeColumn(2);
 //            sheet.autoSizeColumn(3);
